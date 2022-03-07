b88bcc3894818d5ca554aa8478a331d5f531077b
quyen <vanquyen101112@gmail.com>
2022-03-07 20:02:38 +0700

    change cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const routeSong = require('./Routes/song')
 const PORT = process.env.PORT || 4000
 
 app.use(express.json());
-app.use(cors({origin:'https://musiclofi.netlify.app'}));
+app.use(cors());
 
 
 app.use('/auth',routeAuth);
